141d34f01eb58fc9cf4bc2350c134e73b74260d6
Julien Boudry <julien.boudry@gmail.com>
2023-12-04 22:00:39 +0100

    Skig slow architecture test on windows (Pest bug)

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use MammothPHP\WoollyM\Stats\StatsInterface;
 
-test('strict types')->expect('MammothPHP\WoollyM')->toUseStrictTypes();
+test('strict types')->skipOnWindows()->expect('MammothPHP\WoollyM')->toUseStrictTypes();
 
 test('stats modules implements interfaces')->expect('MammothPHP\WoollyM\Stats\Modules')->toImplement(StatsInterface::class);
