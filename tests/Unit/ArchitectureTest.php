2f0b09a096c4720cca4e649082b9b1259e589cfd
Julien Boudry <julien.boudry@gmail.com>
2023-12-05 22:39:00 +0100

    tests: use arch function

--- tests/Unit/ArchitectureTest.php
+++ tests/Unit/ArchitectureTest.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 use MammothPHP\WoollyM\Stats\StatsInterface;
 
-test('strict types')->skipOnWindows()->expect('MammothPHP\WoollyM')->toUseStrictTypes();
+arch('strict types')->skipOnWindows()->expect('MammothPHP\WoollyM')->toUseStrictTypes();
 
-test('stats modules implements interfaces')->expect('MammothPHP\WoollyM\Stats\Modules')->toImplement(StatsInterface::class);
+arch('stats modules implements interfaces')->expect('MammothPHP\WoollyM\Stats\Modules')->toImplement(StatsInterface::class);
