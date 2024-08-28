1f1279062fed098b99a2bd3c1c1fb85458a76d41
Julien Boudry <julien.boudry@gmail.com>
2024-08-28 16:58:10 +0200

    opera performances test

---
+++ tests/Examples/OperasTest.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+use MammothPHP\WoollyM\IO\CSV;
+use MammothPHP\WoollyM\Sort\Desc;
+use MammothPHP\WoollyM\Stats\Modules\{CountDistinctValues, Sum};
+
+# from https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/8LUFN8
+# public domain
+
+beforeEach(function (): void {
+    $this->df = CSV::fromFilePath(__DIR__ . '/opera_performances.csv')->import();
+});
+
+it('has correct columns', function (): void {
+    expect($this->df->columnsNames())->toBe([
+        'season',
+        'iso',
+        'city',
+        'composer',
+        'db',
+        'dd',
+        'nat',
+        'mf',
+        'work',
+        'worknat',
+        'type',
+        'start date',
+        'performances',
+        'production',
+    ]);
+
+});
+
+
+test('rank composers runs', function (): void {
+    $lines = 33_127;
+    expect($this->df)->toHaveCount($lines);
+    expect($this->df->selectAll()->size())->toBe($lines * \count($this->df->columnsNames()));
+
+    expect(
+        $this->df->groupBy(
+            'composer',
+            Sum::col('performances', as: 'total_performances'),
+            CountDistinctValues::col('work', as: 'distincts_work'),
+            CountDistinctValues::col('iso', as: 'distincts_countries'),
+            CountDistinctValues::col('city', as: 'distincts_cities'),
+        )
+            ->orderBy(Desc::col('total_performances'))
+            ->head(10)
+    )->toMatchSnapshot();
+});
